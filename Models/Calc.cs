33877818b435481022efa9d01f2d7c9becde3cc2
Максим Михайлов <KarapooZZ@outlook.com>
2022-01-10 21:37:05 +0300

    Добавьте файлы проекта.

---
+++ Models/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_19_Wpf.Models
+{
+    static class Calc
+    {
+        public static double CalcDl (double r) => 2 * Math.PI * r;
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Zadanie_19_Wpf.Models;
+
+namespace Zadanie_19_Wpf.ViewModels
+{
+    class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double dlina;
+        public double Dlina
+        {
+            get => dlina;
+            set
+            {
+                dlina = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcDlina { get; }
+        private void OnCalcDlinaExecute (object p)
+        {
+            Dlina = Calc.CalcDl(Radius);
+        }
+
+        private bool CanCalcDlinaExecuted (object p)
+        {
+            if (Radius != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public MainWindowViewModel ()
+        {
+            CalcDlina = new RelayCommand(OnCalcDlinaExecute, CanCalcDlinaExecuted);
+        }
+            
+    }
+}
